8fa85f5ade47a4ea0f8e7a3aa187fc82825d4919
JYZHU03 <146981195+JYZHU03@users.noreply.github.com>
2025-05-30 16:26:39 +0000

    Deploying to gh-pages from @ JYZHU03/JYZHU03.github.io@428c7db522c1e61e62db63e75be4cea778daa2aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,8 +448,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-one-paper-on-graph-representation-learning-is-accepted-to",
+          title: 'One paper on graph representation learning is accepted to',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
